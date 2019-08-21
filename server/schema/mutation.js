6e1375aeb7020ccc502164fe58b82ecd52f9a728
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-08-21 16:40:24 +0300

    sing up mutation

---
+++ server/schema/mutation.js
@@ -0,0 +1,18 @@
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const UserType = require('./types/user_type');
+const AuthService = require('../services/auth');
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    email: { type: GraphQLString },
+    password: { type: GraphQLString }
+  },
+  resolve(parentValue, { email, password }, req) {
+    return AuthService.signup({ email, password, req });
+  }
+});
+
+module.exports = mutation;
