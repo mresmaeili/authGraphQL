06996dcac4c15a8db1a3bdaf556263005b5014c0
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-08-21 23:26:00 +0300

    mutations added

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -7,11 +7,34 @@ const AuthService = require('../services/auth');
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    email: { type: GraphQLString },
-    password: { type: GraphQLString }
-  },
-  resolve(parentValue, { email, password }, req) {
-    return AuthService.signup({ email, password, req });
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.signup({ email, password, req });
+      }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({ email, password, req });
+      }
+    }
   }
 });
 
