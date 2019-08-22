8b08c29b8ef364bf2ef9ef6475662d938ecac7e9
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-08-22 18:41:07 +0300

    handling actions in header

--- client/index.js
+++ client/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 
+//allow graphql to send cookies to server
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 });
 
